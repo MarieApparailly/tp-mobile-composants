a1d60258425d037d753a5cef41bf548d901d2717
Marie Appaailly <mapparailly@ensc.fr>
2023-02-20 10:38:05 +0100

    carré bleu

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Hello World ;)</Text>
       <StatusBar style="auto" />
+      <View style={styles.brick}></View>
     </View>
   );
 }
@@ -13,12 +14,24 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
-  
-  text : {
+
+  text: {
     fontSize: 50,
-  }
+  },
+
+  brick: {
+    width: 200,
+    height: 200,
+    backgroundColor: "#68C8DE",
+    alignItems: "center",
+    padding: 10,
+    margin: 30,
+    border: "10 solid",
+    borderColor: "#218197",
+    borderRadius: 20,
+  },
 });
